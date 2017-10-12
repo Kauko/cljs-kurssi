c399ef5737aba79acb6c82718fdf1aee5863872e
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-10-12 16:38:15 +0300

    Add endpoint for fetching a products ratings

--- src/clj/widgetshop/services/products.clj
+++ src/clj/widgetshop/services/products.clj
@@ -18,6 +18,16 @@
                              " GROUP BY p.id, p.name, p.description, p.price")
                         category])))
 
+(defn fetch-ratings-for-product [db product-id]
+  (println (str "Fetch ratings for " product-id))
+  (into []
+        (comp
+          (map #(update % :rating double)))
+        (jdbc/query db [(str "SELECT pr.rating, pr.review"
+                             " FROM product_rating pr"
+                             " WHERE pr.product_id = ?")
+                        product-id])))
+
 (defn fetch-product-categories [db]
   (jdbc/query db ["SELECT c.id, c.name, c.description FROM category c"]))
 
@@ -25,14 +35,17 @@
   component/Lifecycle
   (start [{:keys [db http] :as this}]
     (assoc this ::routes
-           (publish! http
-                     (routes
-                      (GET "/categories" []
-                           (transit-response
-                            (fetch-product-categories db)))
-                      (GET "/products/:category" [category]
-                           (transit-response
-                            (fetch-products-for-category db (Long/parseLong category))))))))
+                (publish! http
+                          (routes
+                            (GET "/categories" []
+                              (transit-response
+                                (fetch-product-categories db)))
+                            (GET "/products/:category" [category]
+                              (transit-response
+                                (fetch-products-for-category db (Long/parseLong category))))
+                            (GET "/ratings/:product" [product]
+                              (transit-response
+                                (fetch-ratings-for-product db (Long/parseLong product))))))))
   (stop [{stop ::routes :as this}]
     (stop)
     (dissoc this ::routes)))
